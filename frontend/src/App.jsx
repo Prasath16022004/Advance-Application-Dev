16ba0fc8d32d1406b59984efe6f674ba86d834f6
Prasath16022004 <prasath16022004@gmail.com>
2024-03-15 10:35:48 +0530

    Day 1-Project Setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,38 @@
-
-
-
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./Components/login";
+import Signup from "./Components/Register";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-      Hello World!
-      </div>
-    </>
-  )
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/"  element={<Login />}></Route>
+          <Route path="/register" element={<Signup />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <div>
+//       Hello World!
+//       </div>
+//     </>
+//   )
+// }
+
+// export default App
