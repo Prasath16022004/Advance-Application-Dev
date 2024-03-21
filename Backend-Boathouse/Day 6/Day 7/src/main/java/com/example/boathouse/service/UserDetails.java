440e031e973b2d42e6f678b4883393e6cad6c60c
Prasath s <110531926+Prasath16022004@users.noreply.github.com>
2024-03-21 20:59:37 +0530

    Day 7-Updated(Service,Controller,Jwt)

---
+++ Backend-Boathouse/Day 6/Day 7/src/main/java/com/example/boathouse/service/UserDetails.java
@@ -0,0 +1,10 @@
+package com.example.boathouse.service;
+
+public class UserDetails {
+
+    public Object getUsername() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getUsername'");
+    }
+
+}
